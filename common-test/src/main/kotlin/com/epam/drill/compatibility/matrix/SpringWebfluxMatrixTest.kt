53e2ad7d2495e076be73782325e8a956656001bd
bodyangug <aseevbogdan5@gmail.com>
2024-03-21 11:24:07 +0200

    chore: clean-up code

--- common-test/src/main/kotlin/com/epam/drill/compatibility/matrix/SpringWebfluxMatrixTest.kt
+++ common-test/src/main/kotlin/com/epam/drill/compatibility/matrix/SpringWebfluxMatrixTest.kt
@@ -23,7 +23,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Configuration
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.reactive.server.WebTestClient
-import org.springframework.test.web.reactive.server.expectBody
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
