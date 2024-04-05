de7fe4868dc1af820e01bb60bcd76a36fe46b4d3
iryabov <to.iryabov@gmail.com>
2024-04-05 15:46:17 +0200

    test: add servlet-5-undertow-2 tests
    EPMDJ-10816

---
+++ servlet-5-undertow-2/src/test/kotlin/com/epam/drill/compatibility/matrix/CompatibilityMatrixTest.kt
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2020 - 2022 EPAM Systems
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.epam.drill.compatibility.matrix
+
+import com.epam.drill.test.compatibility.SimpleHttpServlet5
+import io.undertow.Handlers
+import io.undertow.Undertow
+import io.undertow.servlet.Servlets
+import io.undertow.servlet.Servlets.servlet
+import mu.KotlinLogging
+import org.junit.AfterClass
+import org.junit.BeforeClass
+
+
+class CompatibilityMatrixTest : CleanServerMatrixTest() {
+    override val logger = KotlinLogging.logger {}
+
+    override fun withHttpServer(block: (String) -> Unit) {
+        block("http://localhost:$port/")
+    }
+
+    companion object {
+        private lateinit var server: Undertow
+        private val port = 8888
+
+        @BeforeClass
+        @JvmStatic
+        fun startServer() {
+            val servletBuilder = Servlets.deployment().setClassLoader(
+                CompatibilityMatrixTest::class.java.classLoader
+            )
+                .setDeploymentName("testApp.war")
+                .setContextPath("/")
+                .addServlets(
+                    servlet("SimpleServlet", SimpleHttpServlet5::class.java)
+                        .addMapping("/")
+                )
+            val manager = Servlets.defaultContainer().addDeployment(servletBuilder)
+            manager.deploy()
+            val path = Handlers.path(Handlers.redirect("/"))
+                .addPrefixPath("/", manager.start())
+            server = Undertow.builder()
+                .addHttpListener(port, "localhost")
+                .setHandler(path)
+                .build()
+            server.start()
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun stopServer() {
+            server.stop()
+        }
+    }
+}
