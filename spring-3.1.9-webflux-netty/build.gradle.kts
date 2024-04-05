0a95276f4fcb081cb6def31b8e237496ad109238
Bohdan <bohdan_asieiev@epam.com>
2024-04-05 18:39:46 +0300

    feat: move script to subprojects block

--- spring-3.1.9-webflux-netty/build.gradle.kts
+++ spring-3.1.9-webflux-netty/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.konan.target.HostManager
-import org.jetbrains.kotlin.konan.target.presetName
 import java.net.URI
 
 plugins {
@@ -27,36 +24,6 @@ dependencies {
     testImplementation("io.projectreactor:reactor-test:3.4.10")
     testImplementation(project(":common-test"))
     evaluationDependsOn(":test-agent")
-}
-
-tasks {
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = JavaVersion.current().toString()
-    }
-    test {
-        val pathToBinary: String
-        val pathToRuntimeJar: String
-        val hostPresetName = HostManager.host.presetName
-        val targetJarName = "drill-runtime.jar"
-        val fileName = when {
-            HostManager.hostIsMingw -> "drill_agent.dll"
-            HostManager.hostIsMac -> "libdrill_agent.dylib"
-            else -> "libdrill_agent.so"
-        }
-        val property = providers.systemProperty("test-agent.binaries")
-        if (property.isPresent) {
-            val binariesPath = property.get()
-            pathToBinary = "$binariesPath/$hostPresetName/drill-agentDebugShared/$fileName"
-            pathToRuntimeJar = "$binariesPath/$targetJarName"
-        } else {
-            val buildDir = project(":test-agent").buildDir.path
-            pathToRuntimeJar = "$buildDir/libs/$targetJarName"
-            pathToBinary = "$buildDir/bin/$hostPresetName/drill-agentDebugShared/$fileName"
-        }
-        jvmArgs = listOf(
-            "-agentpath:$pathToBinary=$pathToRuntimeJar"
-        )
-    }
 }
 
 license {
