f5c9720a3a3f6af9402325b2df7a10c6f4a08d6f
Bohdan <bohdan_asieiev@epam.com>
2024-04-10 14:39:57 +0300

    fix: return dependency

--- tests/web-frameworks/servlet-5-jetty-11/build.gradle.kts
+++ tests/web-frameworks/servlet-5-jetty-11/build.gradle.kts
@@ -17,6 +17,7 @@ val microutilsLoggingVersion: String by parent!!.extra
 
 dependencies {
     implementation("io.github.microutils:kotlin-logging-jvm:$microutilsLoggingVersion")
+    implementation("jakarta.servlet:jakarta.servlet-api:5.0.0")
     implementation("org.eclipse.jetty:jetty-server:11.0.15")
     implementation("org.eclipse.jetty:jetty-servlet:11.0.15")
     testImplementation(kotlin("test-junit"))
