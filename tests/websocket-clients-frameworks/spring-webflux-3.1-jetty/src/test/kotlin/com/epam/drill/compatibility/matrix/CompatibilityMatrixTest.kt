7aeca2e12b764982d6b3056b9a379b1151db5f5f
epamrsa <epamrsa@gmail.com>
2024-06-17 18:08:45 +0300

    feat: Implement support for WebSocket connections
    - Added license headers
    - Implemented tests for Spring WebFlux websocket clients
    
    Refs: EPMDJ-10809

--- tests/websocket-clients-frameworks/spring-webflux-3.1-jetty/src/test/kotlin/com/epam/drill/compatibility/matrix/CompatibilityMatrixTest.kt
+++ tests/websocket-clients-frameworks/spring-webflux-3.1-jetty/src/test/kotlin/com/epam/drill/compatibility/matrix/CompatibilityMatrixTest.kt
@@ -15,4 +15,19 @@
  */
 package com.epam.drill.compatibility.matrix
 
-class CompatibilityMatrixTest : SpringWebfluxWebSocketClientMatrixTest()
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.web.reactive.socket.client.StandardWebSocketClient
+import org.springframework.web.reactive.socket.client.WebSocketClient
+
+@ContextConfiguration(classes = [CompatibilityMatrixTest.TestWebSocketClientConfig::class])
+class CompatibilityMatrixTest : SpringWebfluxWebSocketClientMatrixTest() {
+
+    @Configuration
+    open class TestWebSocketClientConfig: AbstractTestWebSocketClientConfig() {
+        @Bean
+        override fun testWebSocketClient(): WebSocketClient = StandardWebSocketClient()
+    }
+
+}
--- tests/websocket-clients-frameworks/spring-webflux-3.1-undertow/src/test/kotlin/com/epam/drill/compatibility/matrix/CompatibilityMatrixTest.kt
+++ tests/websocket-clients-frameworks/spring-webflux-3.1-undertow/src/test/kotlin/com/epam/drill/compatibility/matrix/CompatibilityMatrixTest.kt
@@ -15,4 +15,22 @@
  */
 package com.epam.drill.compatibility.matrix
 
-class CompatibilityMatrixTest : SpringWebfluxWebSocketClientMatrixTest()
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.web.reactive.socket.client.UndertowWebSocketClient
+import org.springframework.web.reactive.socket.client.WebSocketClient
+import org.xnio.OptionMap
+import org.xnio.Xnio
+
+@ContextConfiguration(classes = [CompatibilityMatrixTest.TestWebSocketClientConfig::class])
+class CompatibilityMatrixTest : SpringWebfluxWebSocketClientMatrixTest() {
+
+    @Configuration
+    open class TestWebSocketClientConfig: AbstractTestWebSocketClientConfig() {
+        @Bean
+        override fun testWebSocketClient(): WebSocketClient =
+            UndertowWebSocketClient(Xnio.getInstance().createWorker(OptionMap.EMPTY))
+    }
+
+}
