fd0a33020b72a0584ad503130fcecb71d4c04a44
iryabov <to.iryabov@gmail.com>
2024-10-01 14:52:06 +0200

    chore: change licence plugin

--- tests/test-frameworks/selenium-4/build.gradle.kts
+++ tests/test-frameworks/selenium-4/build.gradle.kts
@@ -1,6 +1,4 @@
 import java.net.URI
-import com.hierynomus.gradle.license.tasks.LicenseCheck
-import com.hierynomus.gradle.license.tasks.LicenseFormat
 
 plugins {
     kotlin("jvm")
@@ -60,19 +58,9 @@ drill {
     }
 }
 
-@Suppress("UNUSED_VARIABLE")
 license {
     headerURI = URI("https://raw.githubusercontent.com/Drill4J/drill4j/develop/COPYRIGHT")
-    val licenseFormatSources by tasks.registering(LicenseFormat::class) {
-        source = fileTree("$projectDir/src").also {
-            include("**/*.kt", "**/*.java", "**/*.groovy")
-            exclude("**/kni", "**/commonGenerated")
-        }
-    }
-    val licenseCheckSources by tasks.registering(LicenseCheck::class) {
-        source = fileTree("$projectDir/src").also {
-            include("**/*.kt", "**/*.java", "**/*.groovy")
-            exclude("**/kni", "**/commonGenerated")
-        }
-    }
+    include("**/*.kt")
+    include("**/*.java")
+    include("**/*.groovy")
 }
