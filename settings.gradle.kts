4bd001873b2912beea4d9ec66ce851951a2285c6
iryabov <to.iryabov@gmail.com>
2024-10-16 14:45:46 +0200

    build: add java version range

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,86 +25,100 @@ pluginManagement {
     }
 }
 
+fun includeForJavaVersion(projectPath: String, javaVersions: IntRange) {
+    val currentJavaVersion = System.getProperty("java.version")
+        .split(".")
+        .let {
+            if (it[0] == "1") it[1].toInt() else it[0].toInt()
+        }
+    if (currentJavaVersion in javaVersions)
+        include(projectPath)
+    else
+        logger.lifecycle("Project :$projectPath is not included as Java is not $javaVersions")
+}
+
+val maxJavaVersion = 21
+
 include("common-test")
 include("stub-server")
 
 //Tests
 //Web Servers
-include("tests:web-servers:jetty-10.0")
-include("tests:web-servers:netty-4.1")
-include("tests:web-servers:tomcat-11.0")
-include("tests:web-servers:tomcat-10.1")
-include("tests:web-servers:undertow-2.3")
+includeForJavaVersion("tests:web-servers:jetty-10.0", 11..maxJavaVersion)
+includeForJavaVersion("tests:web-servers:netty-4.1", 8..maxJavaVersion)
+includeForJavaVersion("tests:web-servers:tomcat-11.0", 11..maxJavaVersion)
+includeForJavaVersion("tests:web-servers:tomcat-10.1", 11..maxJavaVersion)
+includeForJavaVersion("tests:web-servers:undertow-2.3", 11..maxJavaVersion)
 
 //Web Frameworks
 //Http Servlets
-include("tests:web-frameworks:servlet-4-tomcat-9")
-include("tests:web-frameworks:servlet-4-jetty-9")
-include("tests:web-frameworks:servlet-5-tomcat-10")
-include("tests:web-frameworks:servlet-5-jetty-11")
-include("tests:web-frameworks:servlet-5-undertow-2")
-include("tests:web-frameworks:servlet-5-glassfish-7")
-include("tests:web-frameworks:servlet-6-tomcat-11")
+includeForJavaVersion("tests:web-frameworks:servlet-4-tomcat-9", 8..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:servlet-4-jetty-9", 8..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:servlet-5-tomcat-10", 11..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:servlet-5-jetty-11", 17..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:servlet-5-undertow-2", 11..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:servlet-5-glassfish-7", 8..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:servlet-6-tomcat-11", 17..maxJavaVersion)
 //Spring MVC
-include("tests:web-frameworks:spring-mvc-1.5-jetty")
-include("tests:web-frameworks:spring-mvc-1.5-tomcat")
-include("tests:web-frameworks:spring-mvc-1.5-undertow")
-include("tests:web-frameworks:spring-mvc-2.7-jetty")
-include("tests:web-frameworks:spring-mvc-2.7-tomcat")
-include("tests:web-frameworks:spring-mvc-2.7-undertow")
-include("tests:web-frameworks:spring-mvc-3.1-jetty")
-include("tests:web-frameworks:spring-mvc-3.1-tomcat")
-include("tests:web-frameworks:spring-mvc-3.1-undertow")
+includeForJavaVersion("tests:web-frameworks:spring-mvc-1.5-jetty", 8..17)
+includeForJavaVersion("tests:web-frameworks:spring-mvc-1.5-tomcat", 8..17)
+includeForJavaVersion("tests:web-frameworks:spring-mvc-1.5-undertow", 8..17)
+includeForJavaVersion("tests:web-frameworks:spring-mvc-2.7-jetty", 8..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:spring-mvc-2.7-tomcat", 8..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:spring-mvc-2.7-undertow", 8..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:spring-mvc-3.1-jetty", 17..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:spring-mvc-3.1-tomcat", 17..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:spring-mvc-3.1-undertow", 17..maxJavaVersion)
 //Spring WebFlux
-include("tests:web-frameworks:spring-webflux-2.7-jetty")
-include("tests:web-frameworks:spring-webflux-2.7-netty")
-include("tests:web-frameworks:spring-webflux-2.7-tomcat")
-include("tests:web-frameworks:spring-webflux-2.7-undertow")
-include("tests:web-frameworks:spring-webflux-3.1-jetty")
-include("tests:web-frameworks:spring-webflux-3.1-netty")
-include("tests:web-frameworks:spring-webflux-3.1-tomcat")
-include("tests:web-frameworks:spring-webflux-3.1-undertow")
+includeForJavaVersion("tests:web-frameworks:spring-webflux-2.7-jetty", 8..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:spring-webflux-2.7-netty", 8..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:spring-webflux-2.7-tomcat", 8..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:spring-webflux-2.7-undertow", 8..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:spring-webflux-3.1-jetty", 17..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:spring-webflux-3.1-netty", 17..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:spring-webflux-3.1-tomcat", 17..maxJavaVersion)
+includeForJavaVersion("tests:web-frameworks:spring-webflux-3.1-undertow", 17..maxJavaVersion)
 //Apache CXF
-include("tests:web-frameworks:cxf-3.4-jetty")
+includeForJavaVersion("tests:web-frameworks:cxf-3.4-jetty", 8..maxJavaVersion)
 //Jersey
-include("tests:web-frameworks:jersey-2-jetty")
+includeForJavaVersion("tests:web-frameworks:jersey-2-jetty", 8..17)
 
 //Http Clients
 //URLConnection
-include("tests:http-clients:urlconnection")
+includeForJavaVersion("tests:http-clients:urlconnection", 8..maxJavaVersion)
 //Apache HttpClient
-include("tests:http-clients:apache-http-client-4.5")
-include("tests:http-clients:apache-http-client-5.3")
+includeForJavaVersion("tests:http-clients:apache-http-client-4.5", 8..maxJavaVersion)
+includeForJavaVersion("tests:http-clients:apache-http-client-5.3", 8..maxJavaVersion)
 //OkHttp Client
-include("tests:http-clients:okhttp-client-3.12")
-include("tests:http-clients:okhttp-client-3.14")
-include("tests:http-clients:okhttp-client-4.12")
+includeForJavaVersion("tests:http-clients:okhttp-client-3.12", 8..maxJavaVersion)
+includeForJavaVersion("tests:http-clients:okhttp-client-3.14", 8..maxJavaVersion)
+includeForJavaVersion("tests:http-clients:okhttp-client-4.12", 8..maxJavaVersion)
 //Spring RestTemplate
-include("tests:http-clients:spring-resttemplate-4.3")
-include("tests:http-clients:spring-resttemplate-5.3")
+includeForJavaVersion("tests:http-clients:spring-resttemplate-4.3", 8..maxJavaVersion)
+includeForJavaVersion("tests:http-clients:spring-resttemplate-5.3", 8..maxJavaVersion)
 //Spring WebClient
-include("tests:http-clients:spring-webclient-5.3")
-include("tests:http-clients:spring-webclient-6.1")
+includeForJavaVersion("tests:http-clients:spring-webclient-5.3", 8..maxJavaVersion)
+includeForJavaVersion("tests:http-clients:spring-webclient-6.1", 17..maxJavaVersion)
 //Feign Client
-include("tests:http-clients:feign-client-13")
+includeForJavaVersion("tests:http-clients:feign-client-13", 8..maxJavaVersion)
 
 //Asynchronous Communication
 //Service Executor
-include("tests:async:executor-service")
+includeForJavaVersion("tests:async:executor-service", 8..maxJavaVersion)
 //Reactor
-include("tests:async:reactor-3.5")
-include("tests:async:reactor-3.6")
+includeForJavaVersion("tests:async:reactor-3.5", 8..maxJavaVersion)
+includeForJavaVersion("tests:async:reactor-3.6", 8..maxJavaVersion)
 //Spring Task Execution
-include("tests:async:spring-task-execution-3.1")
+includeForJavaVersion("tests:async:spring-task-execution-3.1", 17..maxJavaVersion)
 
 //Test Frameworks
 //JUnit
-include("tests:test-frameworks:junit-4")
-include("tests:test-frameworks:junit-5")
+includeForJavaVersion("tests:test-frameworks:junit-4", 8..maxJavaVersion)
+includeForJavaVersion("tests:test-frameworks:junit-5", 8..maxJavaVersion)
 //TestNG
-include("tests:test-frameworks:testng-6.1")
-include("tests:test-frameworks:testng-7.4")
+includeForJavaVersion("tests:test-frameworks:testng-6.1", 8..maxJavaVersion)
+includeForJavaVersion("tests:test-frameworks:testng-7.4", 8..maxJavaVersion)
 //Selenium
-include("tests:test-frameworks:selenium-4")
+includeForJavaVersion("tests:test-frameworks:selenium-4", 8..maxJavaVersion)
 //Rest Assured
-include("tests:test-frameworks:rest-assured-5.3")
+includeForJavaVersion("tests:test-frameworks:rest-assured-5.3", 8..maxJavaVersion)
